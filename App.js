2b68416fa4aa4323a7bee2d445f39fd76394334b
gideon nnalue <emekannalue@gmail.com>
2020-06-15 18:12:51 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import HelloWorldScene from './scene/HelloWorldSceneAR';
+
+const App = () => {
+  return (
+    <HelloWorldScene />
+  );
+};
+
+export default App;
